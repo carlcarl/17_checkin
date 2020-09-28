a68788620fc367c5dc67194b67d0a62c1b1b16d4
carlcarl <carlcarlking@gmail.com>
2020-09-28 11:14:40 +0800

    Add messages

--- checkin.py
+++ checkin.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 '''
 Checkin script
 '''
 
 import time
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,10 +26,18 @@ def main():
         options.add_argument('--disable-gpu')
 
     chrome = webdriver.Chrome('./chromedriver', chrome_options=options)
+
+    rand_start = random.randint(0, 60)
+
+    print('Start after {} seconds...'.format(rand_start))
+    time.sleep(rand_start)
+
+    print('Open login page...')
     chrome.get(
         "https://scsservices.azurewebsites.net/Login.aspx?CompanyID=QYD547AA59Z53H3CCC27552C"
     )
 
+    print('Do login...')
     account = chrome.find_element_by_id('edtUserID_I')
     password = chrome.find_element_by_id('edtPassword_I')
     login = chrome.find_element_by_id('btnLogin')
@@ -39,14 +48,17 @@ def main():
 
     time.sleep(3)
 
+    print('Navigate to checkin page...')
     chrome.get('https://scsservices.azurewebsites.net/HRM/ATT/AttEmpOnlineSwipe.aspx')
 
     time.sleep(3)
 
+    print('Do checking...')
     checkin = chrome.find_element_by_id('btnOnSwipe')
     checkin.click()
 
     time.sleep(3)
+    print('Done')
 
 if __name__ == '__main__':
     main()
