f7594d8ebd02fd52bae4df28d281d00c0808a2ac
carlcarl <carlcarlking@gmail.com>
2020-09-28 12:15:56 +0800

    Update option

--- checkin.py
+++ checkin.py
@@ -19,10 +19,10 @@ def main():
 
     options = Options()
     options.add_argument("--disable-notifications")
+    options.add_argument('--no-sandbox')
 
     if HIDE_UI:
         options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
 
     chrome = webdriver.Chrome('./chromedriver', chrome_options=options)
